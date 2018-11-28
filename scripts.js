6dd164bfa08f5d82f4025e7308f50f38225134e1
Steve Han <shan61489@gmail.com>
2018-11-28 16:34:02 -0600

    added functionality to rgb ranges

--- scripts.js
+++ scripts.js
@@ -30,8 +30,11 @@ function paintToCanvas() {
     ctx.drawImage(video, 0, 0, width, height); // the 0, 0 are the coordinates for starting at the top left hand of the video
     //take the pixels out
     let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = rgbRange(pixels);
     // mess with the pixels (in this case, the rgb values)
-    pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    //ghosting effect (slow motion 20% speed)
+    // ctx.globalAlpha = 0.2;
     // put the pixels back after messing with them
     ctx.putImageData(pixels, 0, 0);
 
@@ -60,14 +63,59 @@ function takePhoto() {
 }
 
 
-//function that messes with the rgb values 
+// function that messes with the rgb values and emphasizes red
 function redEffect(pixels) {
   for(let i = 0; i < pixels.data.length; i+=4) {
-     pixels.data[i] = pixels.data[i] + 200; // red from rgb
-     pixels.data[i + 1] = pixels.data[i + 1] - 100; // green from rgb
+     pixels.data[i] = pixels.data[i] + 100; // red from rgb
+     pixels.data[i + 1] = pixels.data[i + 1] - 50; // green from rgb
      pixels.data [i + 2] = pixels.data [i + 2] * 0.5; // blue from rgb
   }
   return pixels;
+}
+
+// function that messes with the rgb values and emphasizes green
+function greenEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i] = pixels.data[i] - 25; 
+    pixels.data[i + 1] = pixels.data[i + 1] + 40; 
+    pixels.data [i + 2] = pixels.data [i + 2] * 0.5; 
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 300] = pixels.data[i]; 
+    pixels.data[i + 400] = pixels.data[i + 1]; 
+    pixels.data [i - 450] = pixels.data [i + 2]; 
+  }
+  return pixels;  
+}
+
+function rgbRange(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for(i = 0; i < pixels.data.length; i+=4) {
+    red = pixels.data[i]; 
+    green = pixels.data[i + 1]; 
+    blue = pixels.data [i + 2]; 
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      //take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+  return pixels;  
 }
 
 getVideo();
